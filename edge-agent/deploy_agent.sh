937bb7efca324d857807ea38be305fb99a5e61fc
salehahmed325 <salehahmed904@gmail.com>
2025-09-09 13:54:11 +0600

    Updated deploy script and add fluent-bit.env

--- edge-agent/deploy_agent.sh
+++ edge-agent/deploy_agent.sh
@@ -15,30 +15,11 @@ docker pull salehahmed325/fluent-bit:latest
 
 # Run the fluent-bit container
 echo "Running the latest fluent-bit container......"
-docker run -d \
-  --name fluent-bit \
-  --restart unless-stopped \
-  --user root \
-  -v /var/log:/var/log:ro \
-  -v /var/lib/docker/containers:/var/lib/docker/containers:ro \
-  --group-add 119 \
-  -e CLUSTER_ID=$(grep CLUSTER_ID /home/saleh/ai-devops-platform/edge-agent/config.env | cut -d '=' -f2) \
-  -e API_KEY=$(grep API_KEY /home/saleh/ai-devops-platform/edge-agent/config.env | cut -d '=' -f2) \
-  -e CENTRAL_BRAIN_HOST=ai-devops-platform-alb-dev-547606599.us-east-1.elb.amazonaws.com \
-  -e CENTRAL_BRAIN_PORT=80 \
-  salehahmed325/fluent-bit:latest
+docker run -d --name fluent-bit --restart unless-stopped --user root -v /var/log:/var/log:ro -v /var/lib/docker/containers:/var/lib/docker/containers:ro --group-add 119 --env-file /home/saleh/ai-devops-platform/edge-agent/fluent-bit.env salehahmed325/fluent-bit:latest
 
 # Run the agent container
 echo "Running the latest edge-agent container......"
-docker run -d \
-  --name edge-agent \
-  --restart unless-stopped \
-  -p 8080:8080 \
-  -e TZ=Asia/Dhaka \
-  --env-file /home/saleh/ai-devops-platform/edge-agent/config.env \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  --group-add 119 \
-  salehahmed325/edge-agent:latest
+docker run -d --name edge-agent --restart unless-stopped -p 8080:8080 -e TZ=Asia/Dhaka --env-file /home/saleh/ai-devops-platform/edge-agent/config.env -v /var/run/docker.sock:/var/run/docker.sock --group-add 119 salehahmed325/edge-agent:latest
 
 # Check the container statuses
 echo "Checking container statuses......"
